679331d3c1f3a348788e5c2fb3176d435b9aa34e
mustafatugrul <mustafatugrul034@gmail.com>
2024-07-20 14:43:00 +0300

    Klasör yapısı oluşturuldu.

---
+++ Booking.Entities/Abstractions/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Entities.Abstractions
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedDate { get; set; }
+    }
+}
