472825fcca725fee56ed2c02db45d49f7d488778
Fırat Kılıç <thefiratkilic@hotmail.com>
2024-07-21 13:41:27 +0300

    BookingsRepository Güncellendi.

---
+++ Booking.Business/Validator/BookingGuestValidator.cs
@@ -0,0 +1,19 @@
+﻿using Booking.Entities.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Business.Validator
+{
+    public class BookingGuestValidator : AbstractValidator<BookingGuest>
+    {
+        public BookingGuestValidator() 
+        {
+            RuleFor(b => b.Booking).NotEmpty
+        }
+
+    }
+}
--- Booking.DataAccess/Repository/BookingsRepository.cs
+++ Booking.DataAccess/Repository/BookingsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Booking.DataAccess.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace Booking.DataAccess.Repository
 {
-    public class BookingsRepository
+    public class BookingsRepository : GenericRepository<BookingsRepository>
     {
-
+        public BookingsRepository(ApplicationDbContext context) : base(context)
+        {
+        }
     }
 }
